b9fe6e2d784275b650ee151177bb565ca21987ea
Hosvile <100496294+Hosvile@users.noreply.github.com>
2024-04-14 07:00:02 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,3 @@
-local __function = loadstring(readfile("Security/Init.lua"))
+local __function = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Luarion/main/Security/Init.lua", true))
 
 __function(__function)
