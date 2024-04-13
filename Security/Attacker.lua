ce5a9a5e82eb33f8e8f31e6e5253ec0dd25e0cd0
Hosvile <100496294+Hosvile@users.noreply.github.com>
2024-04-14 06:57:51 +0800

    Update Attacker.lua

--- Security/Attacker.lua
+++ Security/Attacker.lua
@@ -1,1 +1,19 @@
+do
+	game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
+	
+	local oldPrint = print
 
+	--[
+	local old; old = hookfunction(print, newcclosure(function(...)
+		local data = ...
+
+		print(data)
+
+		return old(...)
+	end))
+	--]]
+end
+
+local __function = loadstring(readfile("Security/Init.lua"))
+
+__function(__function)
