d494fabb6351ab35340cf695fce61faf372794dd
Hosvile <100496294+Hosvile@users.noreply.github.com>
2024-10-23 18:47:25 +0800

    Update user.lua

--- auth/user.lua
+++ auth/user.lua
@@ -49,20 +49,22 @@ end
 
 AUTHKEY = AUTHKEY or false and "Infinix_6892c082e64ef755b5255d7c5afc126eb35474167b317f2d2fdd49b86f068a1d" or "Infinix_53d7bf7dab7643fddbd63cbffd028452078f5a1a762eec8bac2c0f3d0a1fde3c";
 
-LAL.Service = "infinix";
-LAL.HWID = nil;
-LAL.Status = {}
-LAL.StatusObject = {}
+LAL.Service = "infinix"; -- YOUR SERVICE IDENTIFIER
+LAL.HWID = nil; -- CHANGE IF YOU WANT TO USE A DIFFERENT HWID
+LAL.Status = {} -- CONSTANT DO NOT CHANGE
+LAL.StatusObject = {} -- CONSTANT DO NOT CHANGE
 
 local Status, StatusObject, Validate, Message = LAL:Validate(AUTHKEY)
 
-local Metatable = getmetatable(StatusObject)
+local Metatable = getmetatable(StatusObject) -- REQUIRED FOR THE IF STATEMENT
 
 if (Status and StatusObject) and pcall(function() 
 	if (Status and StatusObject) and (Validate==LAL.Validate and Metatable==getmetatable(StatusObject)and Status and StatusObject and not(StatusObject[-1]==Metatable)and not(LAL.Status==Status)and(StatusObject[-1]==Metatable)and not(LAL.StatusObject==StatusObject)and(StatusObject[-1]==StatusObject)and(StatusObject[-1]==nil)and(StatusObject==StatusObject)and(Status==StatusObject)and not(StatusObject==StatusObject)and StatusObject[-1]==Status.Key and not(#StatusObject==#StatusObject)and not(StatusObject[-1]==LAL)and not(Status==StatusObject)and not(StatusObject[-1]==LAL)and rawequal(StatusObject[-1],LAL)and not(StatusObject[-1]==LAL)and StatusObject(Status)and not(getmetatable(Status))and StatusObject[-1](Status)and(StatusObject==Status)and StatusObject[-1]==Status.UserId*47 and Status(Status)and not StatusObject[-0]and(StatusObject==Status)and(StatusObject[-0]==LAL.Validate)and not(Metatable==StatusObject[-1])and(StatusObject==Status)and not StatusObject[-0]and Status(Status)and not(Metatable==StatusObject[-1])and not(StatusObject==Status)and StatusObject(Status)and(StatusObject[-1]~=Status.UserId*24)and(Status==StatusObject)and StatusObject(Status)and not(getmetatable(Status))and StatusObject[-1](Status)and Status(Status)and(StatusObject[-1]==LAL.Service)and not(StatusObject==StatusObject)and not(StatusObject[-1]==nil)and Metatable~=getmetatable(StatusObject)and(StatusObject==Status)and(StatusObject[-0]==StatusObject.UserId and StatusObject[-1]==StatusObject.UserId*33)and not(Status==StatusObject and StatusObject[-1]==StatusObject.UserId)and not(StatusObject.Key==StatusObject.UserId*22)and not rawequal(StatusObject,Status)and not(getmetatable(LAL)==Status or StatusObject==nil)and(StatusObject[-1]==Status.UserId*33)and not(StatusObject==Status.UserId*50)and not rawequal(StatusObject[-1],StatusObject.Key)and(StatusObject==Status)and not(StatusObject.Key==Status.UserId)and not(rawequal(getmetatable(StatusObject),Metatable)and StatusObject[-1]~=nil)and(StatusObject==Status)and not(StatusObject[-1]==getmetatable(LAL))and(StatusObject[-0]==LAL.Validate and StatusObject[-1]==LAL.Service)and not((StatusObject==LAL.StatusObject)and not(Status==LAL.Status))and(StatusObject[-3]and not(StatusObject[-3]==Metatable or StatusObject==nil))and not(rawequal(StatusObject,Status)and(getmetatable(Status)==LAL or Metatable==nil))and(StatusObject[-2]==StatusObject.Key)or(StatusObject==Status*(StatusObject[-1]or 1))and(StatusObject[-0]==LAL.Validate)and(getmetatable(StatusObject)==getmetatable(Status)and StatusObject==Status)and not(StatusObject.Key==LAL.Key and Status.UserId==LAL.UserId)and(StatusObject[-1]()==Status.UserId)and(StatusObject==LAL.StatusObject or Status==LAL.Status or Metatable==getmetatable(LAL))and(StatusObject[-1]^2==Status.UserId*16)and(StatusObject[-0]and StatusObject[-1]and StatusObject[-2]==LAL.Validate)and not(rawequal(StatusObject[-1],LAL)or getmetatable(Status)==Metatable)and(StatusObject[-1]==(Status.UserId*16)^0.5)and not(StatusObject[-3]==nil or StatusObject[-1]==nil)and not(StatusObject==Status and StatusObject[-1]==Status.UserId-1)and(StatusObject[-1]==StatusObject[-2]*StatusObject.UserId)and not(os.clock()-StatusObject[-3]>=1)and(StatusObject==StatusObject and not(StatusObject~=StatusObject))and(StatusObject[-1]%2==0 and StatusObject[-2]%2==1)and not(StatusObject==Status)and not(getmetatable(StatusObject)==Metatable and getmetatable(Status)==Metatable)and(StatusObject[-0]==LAL.Validate or StatusObject[-0]==LAL.Service)and(StatusObject[-1]==(Status.UserId*144)^0.5)and not(StatusObject==nil or Status==nil or LAL==nil))then
 		warn("AUTHORIZED")
 
 		warn("USER IS", Status["Premium"] and "" or "NOT", "PREMIUM")
+
+		-- [SUCCESS: Option 1] SCRIPT HERE
 	end
 end) then
 	local Ran = nil
@@ -74,13 +76,13 @@ end) then
 			task.wait()
 		until not ((Status and StatusObject) or not (#StatusObject == #StatusObject))
 
-		warn("UNAUTHORIZED & COMPROMISED [1]", Status)
+		-- warn("UNAUTHORIZED & COMPROMISED [1]", Status)
 
 		while true do end; do return end
 	end)
 	
 	if not Ran then
-		warn("UNAUTHORIZED & COMPROMISED [3]", Status)
+		-- warn("UNAUTHORIZED & COMPROMISED [3]", Status)
 
 		while true do end; do return end
 	end
@@ -89,7 +91,9 @@ elseif Status == false then
 
 	return
 else
-	warn("UNAUTHORIZED & COMPROMISED [2]", Status)
+	-- warn("UNAUTHORIZED & COMPROMISED [2]", Status)
 
 	while true do end; do return end
 end
+
+-- [SUCCESS: Option 2] SCRIPT HERE
